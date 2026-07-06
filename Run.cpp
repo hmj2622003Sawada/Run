7c87f7eb527f15982c7c7a4a2145185625a49637
hmj2622003Sawada <hmj2622003@stu.o-hara.ac.jp>
2026-07-06 16:39:58 +0900

    シーン遷移、BGM、背景、プレイヤー画像兼移動の追加

--- Run.cpp
+++ Run.cpp
@@ -9,17 +9,28 @@ const int FPS = 60;
 const int ITEM_MAX = 0; // 出現する障害物の数
 const int STAGE_DISTANCE = FPS * 60; // ステージの長さ
 const int PLAYER_SPEED = 10; // プレイヤーの速度
-enum { desk, isu, bed}; // 障害物
-enum { TITLE, PLAY, OVER, CLEAR}; // シーン
+enum { desk, isu}; // 障害物
+enum { TITLE, RULE, PLAY, OVER, CLEAR}; // シーン
 int bgY = 0;
 
+// プレイヤーの変数１
+int PlayerX = WIDTH / 2;
+int PlayerY = 600;
+int PlayerW = 79;
+int PlayerH = 118;
+
+
+// 障害物の変数
 
 // グローバル変数
-int imgFloor, imgTukue, imgBed, img; // 背景画像
+int imgPlayer1, imgPlayer2; // プレイヤー画像
+int imgFloor, imgTukue, imgIsu, imgOver, imgClear; // 背景画像
 int imgPlayer, imgEnemy; // プレイヤーと敵の画像
 int bgm, jinOver, jinClear; // 音
 int scene = TITLE; // シーン管理
 int timer = 0; // 時間管理
+bool Flag = 1;
+
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
@@ -39,8 +50,73 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 		// ゲームの根幹
 		
+		switch (scene)
+		{
+		case TITLE:
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.3, "Level !", 0xff0000, 80);
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.7, "Press SPACE", 0xffffff, 30);
+			if (CheckHitKey(KEY_INPUT_SPACE))
+			{
+				scene = RULE;
+			}
+			break;
 
-		ScrollBG(1);
+		case RULE:
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.45, "A:左に移動", 0xffffff, 40);
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.55, "D:右に移動", 0xffffff, 40);
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.7, "Press S Key to start.", 0xffffff, 30);
+			if (CheckHitKey(KEY_INPUT_S))
+			{
+				scene = PLAY;
+			}
+			break;
+
+		case PLAY:
+			ScrollBG(1);
+			// 仮
+			if (CheckHitKey(KEY_INPUT_O))
+			{
+				StopSoundMem(bgm);
+				scene = OVER;
+			}
+			if (CheckHitKey(KEY_INPUT_C))
+			{
+				StopSoundMem(bgm);
+				scene = CLEAR;
+			}
+
+
+			if (Flag == 1)
+			{
+				MovePlayer();
+				PlaySoundMem(bgm, DX_PLAYTYPE_LOOP);
+				// MoveItem();
+			}
+			break;
+
+		case OVER:
+			DrawExtendGraph(0, 0, 720, 1000, imgEnemy, TRUE);
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.5, "GAME OVER", 0xff0000, 80);
+			timer++;
+			if (timer > FPS * 3)
+			{
+				scene = TITLE;
+			}
+			break;
+
+		case CLEAR:
+			DrawExtendGraph(0, 0, 720, 1000, imgClear, FALSE);
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.5, "あなたは逃げ切った", 0xf0f0f0, 80);
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.7, "Press to R key", 0xffffff, 30);
+			DrawTextC(WIDTH * 0.5, HEIGHT * 0.8, "Return TITLE", 0xffffff, 30);
+			if (CheckHitKey(KEY_INPUT_R))
+			{
+				scene = TITLE;
+			}
+			break;
+		}
+
+
 
 
 
@@ -58,9 +134,16 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 void InitGame(void)
 {
+	// プレイヤー画像
+	imgPlayer1 = LoadGraphWithCheck("image/run1.png");
+	imgPlayer2 = LoadGraphWithCheck("image/run2.png");
+
 	// 背景画像
-	imgFloor = LoadGraphWithCheck("image/floor.png");
+	imgFloor = LoadGraphWithCheck("image/FloorNew.png");
 	imgTukue = LoadGraphWithCheck("image/desk.png");
+	imgIsu = LoadGraphWithCheck("image/isu.png");
+	imgOver = LoadGraphWithCheck("image/back.png");
+	imgClear = LoadGraphWithCheck("image/clear.jpg");
 
 	// 背後の敵(画像が見つかり次第コメント解除)
 	imgEnemy = LoadGraphWithCheck("image/back.png");
@@ -81,16 +164,15 @@ void ScrollBG(int spd)
 	if (bgY >= FloorH) bgY = bgY - FloorH;
 	DrawGraph(0, bgY - FloorH, imgFloor, FALSE);
 	DrawGraph(0, bgY, imgFloor, FALSE);
-	DrawExtendGraph(0, 900, 586, 1243, imgEnemy, TRUE);
+	DrawExtendGraph(0, 850, 720, 1243, imgEnemy, FALSE);
+	DrawGraph(PlayerX, PlayerY, imgPlayer1, TRUE);
+	// DrawGraph(PlayerX, PlayerY, imgPlayer2, TRUE);
+	DrawExtendGraph(200, 100,350 , 150, imgIsu, TRUE); // 仮
 }
  
 
 
 
-void DrawTextC(int x, int y, const char* txt, int col, int siz)
-{
-	SetFontSize(siz);
-}
 
 // 画像読み込み失敗通知
 int LoadGraphWithCheck(const char* file)
@@ -114,4 +196,39 @@ int LoadSoundMemWithCheck(const char* file)
 	}
 }
 
+void DrawTextC(int x, int y, const char* txt, int col, int siz)
+{
+	SetFontSize(siz);
+	int strWidth = GetDrawStringWidth(txt, strlen(txt));
+	x -= strWidth / 2;
+	y -= siz / 2;
+	DrawString(x + 1, y + 1, txt, 0x000000);
+	DrawString(x, y, txt, col);
+}
+
+void MovePlayer()
+{
+	// プレイヤーの処理
+	if (CheckHitKey(KEY_INPUT_A) == 1) // 左入力
+	{
+		PlayerX = PlayerX - 10;
+		if (PlayerX < PlayerW / 2 + 130) PlayerX = PlayerW / 2 + 130;
+	}
+	if (CheckHitKey(KEY_INPUT_D) == 1) // 右入力
+	{
+		PlayerX = PlayerX + 10;
+		if (PlayerX > WIDTH - 160 - PlayerW / 2) PlayerX = WIDTH - 160 - PlayerW / 2;
+	}
+}
+
+int SetItem(int x, int y, int ptn, int img)
+{
+
+}
+
+
+void MoveItem()
+{
+
+}
 // 724 * 2172 画像のサイズ
--- RunGame.h
+++ RunGame.h
@@ -12,3 +12,7 @@ void ScrollBG(int sod);
 void DrawTextC(int x, int y, const char* txt, int col, int siz);
 int LoadGraphWithCheck(const char* file);
 int LoadSoundMemWithCheck(const char* file);
+void DrawTextC(int x, int y, const char* txt, int col, int siz);
+void MovePlayer(void);
+int SetItem(int x, int y, int ptn, int img);
+void MoveItem(void);
